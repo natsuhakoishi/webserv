befc87aa3201584e628b3b1589096ef8e8ad6498
Natsuha <ethanyap8@gmail.com>
2025-06-11 19:31:17 +0800

    very basic Tcp server setup done

---
+++ srcs/server/main.cpp
@@ -0,0 +1,12 @@
+#include "../../includes/TcpServer.hpp"
+
+int main() {
+    std::vector<int> ports;
+    ports.push_back(8080);
+    ports.push_back(9090);
+
+    TcpServer server(ports);
+    server.runServer();
+
+    return 0;
+}
