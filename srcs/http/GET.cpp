c3d26a452aaf20476924854ed2ce05870a487503
yabi <hin050924@gmail.com>
2025-06-12 04:32:30 +0800

    add cat meme for fun

--- srcs/http/GET.cpp
+++ srcs/http/GET.cpp
@@ -5,10 +5,10 @@ string Http::GetContentType(string c)
     // cout << "eeeeeeeee:" << c.substr(c.length() - 5) << endl;
     if (!c.substr(c.length() - 5).compare(".html")) return "text/html";
     if (!c.substr(c.length() - 4).compare(".png")) return "image/png";
+    if (!c.substr(c.length() - 4).compare(".gif")) return "image/gif";
     return "text/plain";
     // return "text/csv";
     // return "text/css";
-    // return "image/gif";
 }
 
 string Http::getContent(string file)
